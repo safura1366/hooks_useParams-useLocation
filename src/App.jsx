0996a3d0227f452a32578a1a4aa8dd8543b57b68
safura <sfwraamarLw@gmail.com>
2025-11-08 09:39:23 +0330

    componnet Navigate

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import UserIndex from "./pages/users/Index";
-import {BrowserRouter,Routes,Route} from "react-router";
+import {BrowserRouter,Routes,Route, Navigate} from "react-router";
 import UserShow from "./pages/users/Show";
+import { useState } from "react";
 function App() {
+  const [login,setLogin]=useState(true)
   return (
     <>
       <BrowserRouter>
       <Header/>
         <Routes>
           <Route path="/" element={<Home/>}/>
-          <Route path="/users" element={<UserIndex/>}/>
+          <Route path="/users" element={login ? <UserIndex/> :<Navigate to ="/"/>}/>
           {/* urlparams */}
             <Route path="/users/:id" element={<UserShow/>}/> 
             <Route path="*" element={<NotFound/>}/>
--- src/pages/users/Index.jsx
+++ src/pages/users/Index.jsx
@@ -2,10 +2,7 @@ import React from 'react'
 import { useLocation } from 'react-router'
 
 export default function UserIndex() {
- const {search}= useLocation()
- const query= new URLSearchParams(search)
- console.log(query.get('name'))
- console.log(search)
+
   return (
     <div>
       <h1> user index page</h1>
